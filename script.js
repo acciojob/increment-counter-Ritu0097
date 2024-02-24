554615207e8356eec523ec57c12a5b67f270564a
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-24 14:24:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const paraincre=document.getElementById('counter');
+const clickbutton=document.getElementById('eg1');
+
+clickbutton.addEventListener('click',() => {
+	let counterVal = parseInt(paraincre.textContent);
+	alert(counterVal);
+	counterVal++;
+	paraincre.textContent=counterVal;
+});
